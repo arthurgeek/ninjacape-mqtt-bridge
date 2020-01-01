eb381d3b6204fecc3dcdbc38909f2dff9faebddd
Blake Samuels <16077190+tazard@users.noreply.github.com>
2020-01-01 19:11:47 +0800

    Clean up, renames, overhaul, readme overhaul. Remove openhab scripts and unused init script.

--- README.md
+++ README.md
@@ -1,29 +1,70 @@
-# ninjacape-mqtt-bridge
-<html>
-<h2>Python script for grabbing JSON data over serial from the NinjaCape and publishing it as MQTT messages</h2>
+# Ninja Cape MQTT Bridge
+
+## What it is
+
+Python script for grabbing JSON data over serial from the NinjaCape and 
+publishing it as MQTT messages.
+
+## Ninja Blocks
+
+Ninja Blocks is a now defunct software project that typically used
+a Beagle Bone Black (BBB) and Arduino based Ninja Cape to do smart home
+(home automation, IOT) type things. It was web based using cloud severs
+that are long dead now.
+
+The usefulness of the Ninja Cape is that it had 433MHz wireless comms
+and talked to a variety of sensors that came with the Ninja Blocks kit.
+These included door bell (button), proximity sensor, temperature sensor,
+PIR sensor.
+
+The Ninja Cape uses normal 9600 baud serial to talk with BBB to report
+433Mhz messages it receives from sensors.
+
+The script in this repo replaces all that Ninja Blocks code with a 
+simple script that reads the serial messages from Ninja Cape and 
+publishes them with MQTT so that you can integrate with Home Assistant
+or OpenHAB.
+
+## Requirements
 
 You need to have an MQTT broker installed, such as http://mosquitto.org/
 
-<br><b><a href="https://github.com/perrin7/ninjacape-mqtt-bridge/wiki">See the wiki for more information</a></b>
+It you use Hassio for Home Assistant, you can install its MQTT plugin.
 
-<h3>MQTT messages are structured as follows:</h3>
-<ul>
-<li>Messages received on 433Mhz as published to:
-<br>/ninjaCape/input/<i>DeviceID</i>
-<br>payload: <i>DeviceData</i>
-</li>
-<li>
-Messages to be sent out on 433Mhz should be pushlished to:
-<br>/ninjaCape/output/<i>DeviceID</i>
-<br>payload: <i>DeviceData</i>
-</li>
-<li>The script subscribes to all updates on /ninjaCape/output/#</li>
-</ul>
+You need a Python 3.5 environment on the target device. It can be a 
+Ninja Blocks Beagle Bone Black or Raspberry Pi.
 
-<h2>TODO List</h2>
-<ul><li>Make it daemon-ised.  At the moment I'm just running it in a 'screen' instance.  </li>
-<li>Catch invalid JSON messages and throw an error, instead of just ignoring it.</li>
-<li><a href="https://discuss.ninjablocks.com/t/openhab-vs-open-source-sphere/3680/55">Update the MQTT message to have an additional layer for the 'gid' instead of assuming its zero. </a></li>
-<li>Make the MQTT topic configurable, some brokers (i.e. eventghost) require a <i>leading</i> slash to work</li>
-</ul>
-</html>
+I used BBB and put a fresh Debian image on it with SD card.
+
+The python script requires python libraries for PySerial and Paho MQTT
+
+```
+pip install pyserial
+pip install paho-mqtt
+```
+
+## MQTT Details
+MQTT messages are structured as follows:
+
+* Messages received on 433Mhz as published to:
+  * topic = `/ninjaCape/input/<DeviceID>` 
+  * payload = `DeviceData`
+
+* Messages to be sent to ninja cape should be published to:
+  * topic = `/ninjaCape/output/<DeviceID>` 
+  * payload = `DeviceData`
+  * These messages are only to change LEDs so far as I know (status and eyes)
+  * The script subscribes to all updates on `/ninjaCape/output/#`
+
+```
+Device Type   Device
+11            Generic switch device (Sockets, Buttons, PIRs)
+30            Humidity Sensor
+31            Temperature Sensor
+999           NinjaBlock Status Light
+1007          NinjaBlock Eyes
+```
+
+## TODO List
+* Make it daemon-ised.  At the moment I'm just running it in a 'screen' instance.
+* Catch invalid JSON messages and throw an error, instead of just ignoring it.
---
+++ ninja_cape_mqtt_bridge.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+#
+# used to interface the NinjaCape to home assistant via MQTT
+# - reads data from serial port and publishes on MQTT client
+# - writes data to serial port from MQTT subscriptions
+#
+# - uses the Python MQTT client from the Mosquitto project http://mosquitto.org (now in Paho)
+#
+# https://github.com/tazard/ninjacape-mqtt-bridge
+# perrin7, edits by tazard 2019-12-30
+
+# TODO: Consider handling SIGTERM --
+#  https://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully
+#  https://stackabuse.com/handling-unix-signals-in-python/
+
+import json
+import subprocess
+import threading
+import time
+
+import paho.mqtt.client as mqtt
+import serial
+
+# Settings
+serial_dev_path = '/dev/ttyO1'  # for BBB
+# serial_dev_path = '/dev/ttyAMA0' # for RPi
+
+broker = "hassio"  # mqtt broker
+port = 1883  # mqtt broker port
+
+# Pins for serial UART 
+# (on BBB A5C with 2019-08 image requires these to be set)
+# Set to empty list if no pins need to be configured on your platform
+uart_pins = [
+    # "P9.26",
+    # "P9.24",
+]
+
+debug = True  # Debug printing
+dummy_serial = True  # Fake serial for testing
+
+# buffer of data to output to the serial port
+outputData = []
+
+
+class FakeSerial:
+    """Fake serial object for local testing"""
+    def flushInput(self):
+        pass
+
+    def readline(self):
+        while True:
+            pass
+
+    def write(self, message):
+        pass
+
+
+def mqtt_on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        # rc 0 successful connect
+        print("Connected")
+    else:
+        print("Connection failed, rc={}".format(rc))
+        raise Exception
+    # subscribe to the output MQTT messages
+    client.subscribe("ninjaCape/output/#")
+
+
+def mqtt_on_publish(client, userdata, mid):
+    if debug:
+        print("Published. mid:", mid)
+
+
+def mqtt_on_subscribe(client, userdata, mid, granted_qos):
+    if debug:
+        print("Subscribed. mid:", mid)
+
+
+def mqtt_on_ninja_cape_output(client, userdata, msg):
+    """Queue message to be sent to ninja cape over serial"""
+    if debug:
+        print("Output Data: ", msg.topic, "data:", msg.payload)
+    # add to outputData list
+    outputData.append(msg)
+
+
+def mqtt_on_unhandled_message(client, userdata, message):
+    if debug:
+        print("Unhandled Message Received: ", message.topic, message.paylod)
+
+
+def cleanup(ser, mqtt):
+    """Called on exit to close serial and disconnect MQTT"""
+    print("Ending and cleaning up")
+    ser.close()
+    mqtt.loop_stop()
+
+
+def mqtt_to_json_output(mqtt_message):
+    """convert mqtt_message into a json string for ninja cape serial"""
+    topics = mqtt_message.topic.split('/')
+    # JSON message in ninjaCape form
+    json_data = '{"DEVICE": [{"G":"0","V":0,"D":' + str(topics[2]) + ',"DA":"' + str(mqtt_message.payload) + '"}]})'
+    return json_data
+
+
+def serial_read_and_publish(ser, mqtt):
+    """thread for reading serial data and publishing to MQTT client"""
+    ser.flushInput()
+
+    while True:
+        line = ser.readline()  # this is blocking
+        if debug:
+            print()
+            print("Received from ninja cape:", line.replace('\r', '').replace('\n', ''))
+
+        # split the JSON packet up here and publish on MQTT
+        json_data = json.loads(line)
+
+        try:
+            device = str(json_data['DEVICE'][0]['D']) + "_" + str(json_data['DEVICE'][0]['G'])
+            message = str(json_data['DEVICE'][0]['DA'])
+        except KeyError as e:
+            print("Error while extracting device or message from received JSON data: {}".format(e))
+        else:
+            # No exceptions - data ok
+            topic = "ninjaCape/input/" + device
+            print("Publishing MQTT: topic='{}', message='{}'".format(topic, message))
+            mqtt.publish(topic, message)
+
+
+def main():
+    if len(uart_pins) > 0:
+        print("Setting up UART pins")
+        for pin in uart_pins:
+            command = "config-pin {} uart".format(pin)
+            print(" running: {}".format(command))
+            subprocess.check_call(command, shell=True)
+
+    # Connect serial port
+    if not dummy_serial:
+        print("Connecting... {}".format(serial_dev_path))
+        # timeout 0 for non-blocking. Set to None for blocking.
+        ser = serial.Serial(serial_dev_path, 9600, timeout=None)
+    else:
+        ser = FakeSerial()
+
+    # create an mqtt client
+    mqtt_client = mqtt.Client("ninjaCape")
+
+    # attach MQTT callbacks
+    mqtt_client.on_connect = mqtt_on_connect
+    mqtt_client.on_publish = mqtt_on_publish
+    mqtt_client.on_subscribe = mqtt_on_subscribe
+    mqtt_client.on_message = mqtt_on_unhandled_message
+    mqtt_client.message_callback_add("ninjaCape/output/#", mqtt_on_ninja_cape_output)
+
+    # connect to broker
+    mqtt_client.username_pw_set("MQTT_user", "-")
+    mqtt_client.connect(broker, port, 60)
+
+    # Thread for MQTT client
+    mqtt_client.loop_start()
+
+    # Thread for reading serial from ninja cape
+    serial_thread = threading.Thread(target=serial_read_and_publish, args=(ser, mqtt_client))
+    serial_thread.daemon = True
+    serial_thread.start()
+
+    try:
+        # main thread
+        while True:
+            # writing to serial port if there is data available
+            if len(outputData) > 0:
+                # print "***data to OUTPUT:",mqtt_to_JSON_output(outputData[0])
+                ser.write(mqtt_to_json_output(outputData.pop()))
+
+            time.sleep(0.5)
+
+    # handle deliberate closure gracefully
+    except KeyboardInterrupt:
+        print("Interrupt received")
+        cleanup(ser, mqtt_client)
+
+
+if __name__ == "__main__":
+    main()
